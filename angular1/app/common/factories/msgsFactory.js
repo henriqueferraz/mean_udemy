b1678eeca619f3b88fd2b4e80a91ea164eecb6f1
Henrique Ferraz <henriquefrerraz@ofnet.com.br>
2017-09-30 08:31:27 -0300

    30/10 8:31

---
+++ angular1/app/common/factories/msgsFactory.js
@@ -0,0 +1,11 @@
+(function() {
+    angular.module("primeiraApp").factory("msg", [
+        'toastr',
+      MsgsFactory
+    ]);
+    function MsgsFactory(toastr) {
+        function addMsg(msgs, title, method) {
+          
+      }
+  }  
+});
