b5a8a1f7a0d1352bdaa78a0f4280c9154f2115f7
Henrique Ferraz <henriquefrerraz@ofnet.com.br>
2017-10-02 16:16:19 -0300

    02/10 16:16

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,16 +1,76 @@
-(function() {
+(function () {
   angular
     .module("primeiraApp")
-    .controller("BillingCycleCtrl", ["$http", BillingCycleController]);
-  function BillingCycleController($http) {
+    .controller("BillingCycleCtrl", [
+      "$http",
+      "msgs",
+      "tabs",
+      BillingCycleController
+    ]);
+
+  function BillingCycleController($http, msgs, tabs) {
     const vm = this;
-    vm.create = function() {
-      const url = "http://localhost:3003/api/billingCycles";
-      $http.post(url, vm.billingCycle).then(function(response) {
-        vm.billingCycle = { credits: [{}], debits: [{}] };
+    const url = "http://localhost:3003/api/billingCycles";
+    const selectUrl = `${url}/${vm.billingCycle._id}`;
+    vm.refresh = function () {
+      $http.get(url).then(function (response) {
+        vm.billingCycle = {};
         vm.billingCycles = response.data;
-        console.log("Sucesso!");
+        tabs.show(vm, {
+          tabList: true,
+          tabCreate: true
+        });
       });
     };
+    vm.create = function () {
+      $http
+        .post(url, vm.billingCycle)
+        .then(function (response) {
+          vm.refresh();
+          msgs.addSuccess("Operação realizada com sucesso!");
+        })
+        .catch(function (resp) {
+          msgs.addError(resp.data.errors);
+        });
+    };
+
+    vm.showTabUpdate = function (billingCycle) {
+      vm.billingCycle = billingCycle;
+      tabs.show(vm, {
+        tabUpdate: true
+      });
+    };
+
+    vm.showTabDelete = function (billingCycle) {
+      vm.billingCycle = billingCycle;
+      tabs.show(vm, {
+        tabDelete: true
+      });
+    };
+
+    vm.update = function () {
+      $http
+        .put(selectUrl, vm.billingCycle)
+        .then(function (response) {
+          vm.refresh();
+          msgs.addSuccess("Operação realizada com sucesso");
+        })
+        .catch(function (data) {
+          msgs.addError(data.errors);
+        });
+    }
+
+    vm.delete = function () {
+      $http
+        .delete(selectUrl, vm.billingCycle)
+        .then(function (response) {
+          vm.refresh();
+          msgs.addSuccess("Operação realizada com sucesso");
+        })
+        .catch(function (data) {
+          msgs.addError(data.errors);
+        });
+    };
+    vm.refresh();
   }
 })();
--- backend/config/database.js
+++ backend/config/database.js
@@ -5,3 +5,8 @@ mongoose.Promise = global.Promise;
 let url = "mongodb://henriqueferraz:lai7452@ds127101.mlab.com:27101/ofnet";
 
 module.exports = mongoose.connect(url, { useMongoClient: true });
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
